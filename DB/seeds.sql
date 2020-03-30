0993ac9194cb1ddb3f113827ef91b51d5e8e1b0b
Christian Sanchez <cas81695@gmail.com>
2020-03-30 00:43:39 -0400

    added config files

---
+++ DB/seeds.sql
@@ -0,0 +1,40 @@
+INSERT INTO burger
+    (burger_name)
+VALUES
+    ('The Wolverine');
+INSERT INTO burger
+    (burger_name)
+VALUES
+    ('Vegeta Veggie');
+INSERT INTO burger
+    (burger_name)
+VALUES
+    ('Black Cat');
+INSERT INTO burger
+    (burger_name)
+VALUES
+    ('Double Trouble');
+INSERT INTO burger
+    (burger_name)
+VALUES
+    ('Hamburger');
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('The Impossible', true);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Old School', true);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('The Spider-Man', true);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Triple Terror', true);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('The Iron-Man', true);
